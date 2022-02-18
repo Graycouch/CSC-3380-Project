466c5287541ce93e55619d447ac13f0059597e20
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-02-18 10:15:07 -0600

    Added the backend code for the project

---
+++ Project Code/Node Rest Api/Models/User.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            min: 3,
+            max: 20,
+            unique: true
+        },
+
+        email: {
+            type: String,
+            require: true,
+            max: 50,
+            unique: true,
+        },
+
+        password: {
+            type: String,
+            require: true,
+            min: 6,
+        },
+
+        steamLink: {
+            type: String,
+            max: 100,
+            default: ""
+        },
+
+        playstationLink: {
+            type: String,
+            max: 100,
+            default: ""
+        },
+
+        xboxLink: {
+            type: String,
+            max: 100,
+            default: ""
+        },
+
+        profilePicture: {
+            type: String,
+            default: ""
+        },
+
+        coverPicture: {
+            type: String,
+            default: ""
+        },
+
+        followers: {
+            type: Array,
+            default: []
+        },
+
+        following: {
+            type: Array,
+            default: []
+        },
+
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+
+        description: {
+            type: String,
+            max: 100
+        },
+
+        city: {
+            type: String,
+            max: 50
+        },
+
+        from: {
+            type: String,
+            max: 50
+        },
+
+        relationship: {
+            type: Number,
+            enum: [1, 2, 3]
+        },
+    },
+
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
