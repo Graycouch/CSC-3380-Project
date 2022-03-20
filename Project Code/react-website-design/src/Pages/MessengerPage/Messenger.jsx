f8d1ff2e16bf9fbdfd31f555c93b28d64f54fe53
tmitc59 <tmitc59@lsu.edu>
2022-03-20 08:55:48 -0500

    Implementing Chat

--- Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
+++ Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
@@ -1,12 +1,37 @@
-import "./messenger.css"
-import Topbar from "../../Components/Topbar/Topbar"
+import "./Messenger.css"
+import Topbar from "../../component/topbar/Topbar"
 
-export default function Messenger() {
+export default function Messenger(){
     return(
         <>
             <Topbar/>
             <div className = "messenger">
-                heloo!    
+                <div className = "chatMenu">
+                    <div className="chatMenuWrapper">
+                        <input placeholder = "Search for friends" className = "chatMenuInput"/>
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                    </div>
+                </div>
+                <div className = "chatBox">
+                    <div className="chatBoxWrapper">
+                        <div className="chatBoxTop">
+                            <Message />
+                            <Message own = {true}/>
+                            <Message />
+                        </div>
+                        <div className="chatBoxBottom">
+
+                        </div>
+                    </div>
+                </div>
+                <div className = "chatOnline">
+                    <div className="chatOnlineWrapper">
+                        online
+                    </div>
+                </div>
             </div>
         </>
     )
