ea7e62cfe50500142a6f9176cb0e3f75636638b2
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2022-03-24 14:01:49 -0500

    More

--- Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
+++ Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
@@ -1,38 +1,11 @@
-import "./Messenger.css"
-import Topbar from "../../component/topbar/Topbar"
+import "./messenger.css"
+import Topbar from "../../Components/Topbar/Topbar"
 
-export default function Messenger(){
+export default function Messenger() {
     return(
         <>
             <Topbar/>
-            <div className = "messenger">
-                <div className = "chatMenu">
-                    <div className="chatMenuWrapper">
-                        <input placeholder = "Search for friends" className = "chatMenuInput"/>
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                    </div>
-                </div>
-                <div className = "chatBox">
-                    <div className="chatBoxWrapper">
-                        <div className="chatBoxTop">
-                            <Message />
-                            <Message own = {true}/>
-                            <Message />
-                        </div>
-                        <div className="chatBoxBottom">
-
-                        </div>
-                    </div>
-                </div>
-                <div className = "chatOnline">
-                    <div className="chatOnlineWrapper">
-                        online
-                    </div>
-                </div>
-            </div>
+            <div className = "messenger"> </div>
         </>
     )
 }
