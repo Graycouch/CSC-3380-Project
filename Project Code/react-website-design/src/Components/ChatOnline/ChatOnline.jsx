f87cee5f990fb8dee6e1a3c1f5c18bbeb06faa8a
tmitc59 <tmitc59@lsu.edu>
2022-03-25 00:21:22 -0500

    New Api Files

---
+++ Project Code/react-website-design/src/Components/ChatOnline/ChatOnline.jsx
@@ -0,0 +1,14 @@
+import "./chatOnline.css";
+
+export default function ChatOnline() {
+    return(
+        <div className="chatOnlineFriend">
+            <div className="chatOnlineImgContainer">
+                <img className="chatOnlineImg"
+                src="" alt="" />
+                <div className="chatOnlineBadge"></div>
+            </div>
+            <span className="chatOnlineName">John Doe</span>
+        </div>
+    )
+}
--- Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
+++ Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
@@ -1,11 +1,55 @@
 import "./messenger.css"
 import Topbar from "../../Components/Topbar/Topbar"
+import Conversation from "../../Components/Conversation/Conversation"
+import Message from "../../Components/Message/Message"
 
 export default function Messenger() {
     return(
         <>
             <Topbar/>
-            <div className = "messenger"> </div>
+            <div className = "messenger">
+                <div className="chatMenu">
+                    <div className="chatMenuWrapper">
+                        <input placeholder="Search for friends" className="chatMenuInput"/>
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                    </div>
+                </div>
+                <div className="chatBox">
+                    <div className="chatBoxWrapper">
+                        <div className="chatBoxTop">
+                            <Message />
+                            <Message own={true}/>
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                        </div>
+                        <div className="chatBoxBottom">
+                            <textarea className="chatMessageInput" placeholder="write something..."></textarea>
+                            <button className="chatSubmitButton">Send</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="chatOnline">
+                    <div className="chatOnlineWrapper">
+                        <ChatOnline />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
