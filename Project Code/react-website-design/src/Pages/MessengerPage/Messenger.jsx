5226e75309a2422b1c8a3a9014817098f0745b17
JaylonL19 <jlong58@lsu.edu>
2022-03-27 13:43:26 -0500

    Messenger changes

--- Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
+++ Project Code/react-website-design/src/Pages/MessengerPage/Messenger.jsx
@@ -3,8 +3,60 @@ import Topbar from "../../Components/Topbar/Topbar"
 import Conversation from "../../Components/Conversation/Conversation"
 import Message from "../../Components/Message/Message"
 import ChatOnline from "../../Components/ChatOnline/ChatOnline"
+import { useContext, useEffect, useRef, useState } from "react"
+import { AuthContext } from "../../Context/AuthContext"
+import axios from "axios"
 
 export default function Messenger() {
+    const [conversation, setConversations] = useState([]);
+    const [currentChat, setCurrentChat] = useState(null);
+    const [messages, setMessages] = useState([])
+    const [newMessage, setNewMessages] = useState("")
+    const {user} = useContext(AuthContext);
+    const scrollRef = useRef();
+
+    useEffect(() =>{
+        const getConversations = async ()=>{
+            try{
+            const res =await axios.get("/conversations/"+user._id)
+           setConversations(res.data);
+        }
+        catch (err){
+            console.log(err);
+        }
+        };
+        getConversations();
+    },[user._id])
+
+    useEffect(()=>{
+        const getMessages=async ()=>{
+            try{
+            const res = await axios.get("/messages/"+currentChat?._id);
+            setMessages(res.data);
+        }
+        catch(err){
+            console.log(err)
+        }
+    };
+    getMessages();
+    },[currentChat]);
+
+    console.handleSubmit = async (e) =>{
+        e.preventDefault();
+        const message = {
+            sender: user._id,
+            text: newMessage,
+            conversationId: currentChat._id,
+        };
+        try{
+            const res = await axios.post("/messages", message)
+            setMessages([...messages,res.data])
+            setNewMessage("")
+        }catch(err){
+            console.log(err);
+        }
+    };
+
     return(
         <>
             <Topbar/>
@@ -12,37 +64,37 @@ export default function Messenger() {
                 <div className="chatMenu">
                     <div className="chatMenuWrapper">
                         <input placeholder="Search for friends" className="chatMenuInput"/>
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
+                        {conversation.map(c=>(
+                            <div onClick={()=>setCurrentChat(c)}>    
+                            <Conversation conversation={c} currentUser={user}/>
+                            </div>
+                        ))}
                     </div>
                 </div>
                 <div className="chatBox">
                     <div className="chatBoxWrapper">
+                        {
+                            currentChat ?
+                        <>
                         <div className="chatBoxTop">
-                            <Message />
-                            <Message own={true}/>
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
-                            <Message />
+                            {messages.map(m=>(
+                            <Message message={m} own={m.sender === user._id}/>
+                            ))}
                         </div>
                         <div className="chatBoxBottom">
-                            <textarea className="chatMessageInput" placeholder="write something..."></textarea>
-                            <button className="chatSubmitButton">Send</button>
+                            <textarea className="chatMessageInput" 
+                            placeholder="write something..."
+                            onChange={(e)=>setNewMessage(e.target.value)}
+                            value={newMessage}
+                            ></textarea>
+                            <button className="chatSubmitButton" onClick={handleSubmit}>Send</button>
                         </div>
+                        </>
+: (  //supposed to be ):( but didn't work
+                        <span className ="noConversationText">
+                            Open a conversation to start a chat.
+                            </span>
+                        )}
                     </div>
                 </div>
                 <div className="chatOnline">
