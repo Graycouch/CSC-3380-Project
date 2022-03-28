f4213568a1d69087db70e300392efce729189034
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2022-03-28 01:08:22 -0500

    Update index.js

--- Project Code/socket/index.js
+++ Project Code/socket/index.js
@@ -3,6 +3,8 @@ const io = require("socket.io")(8900,{
     origin:"http://localhost:3000"
   }
 };
+                                
+let users = [];
 
 io.on("connection", (socket) => {
   console.log("a user connected.")
