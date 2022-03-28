7d3ac55ecc3071f2d86cde255aea1c6e52300aa0
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2022-03-28 01:17:53 -0500

    Update index.js

--- Project Code/socket/index.js
+++ Project Code/socket/index.js
@@ -6,7 +6,15 @@ const io = require("socket.io")(8900,{
                                 
 let users = [];
 
+const addUser = (userId,socketId) =>{
+  !users.some(user=>user.userId === userId) &&
+    users.push({userId, socketId });
+}
+
 io.on("connection", (socket) => {
   console.log("a user connected.")
-  io.emit("welcome","hello, this is socket server!")
+  socket.on("addUser",(userId) =>{
+     addUser(userID, socket.id);      
+    io.emit("getUsers", users);
+  });
 })
