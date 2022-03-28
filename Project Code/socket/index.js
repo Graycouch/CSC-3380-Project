2fa624fa299b0e6fb3362302a9f7f29d3b0b00dc
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2022-03-28 10:37:35 -0500

    Fixed some minor bugs in the code

--- Project Code/socket/index.js
+++ Project Code/socket/index.js
@@ -1,37 +1,47 @@
-const io = require("socket.io")(8900,{
-  cors:{
-    origin:"http://localhost:3000"
-  }
-};
-                                
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
 let users = [];
 
-const addUser = (userId,socketId) =>{
-  !users.some(user=>user.userId === userId) &&
-    users.push({userId, socketId });
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
 };
-const removeUser = (socketId) =>{
+
+const removeUser = (socketId) => {
   users = users.filter((user) => user.socketId !== socketId);
 };
 
-socket.on("sendMessage", ({userId,receiverId,text})=>{
-  const user = getUser(userId);
-  io.to(user.socketId.emit("getMessage", {
-    userId,
-    text,
-  })
-});
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 
 io.on("connection", (socket) => {
-  console.log("a user connected.")
-  socket.on("addUser",(userId) =>{
-     addUser(userID, socket.id);      
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
     io.emit("getUsers", users);
   });
-  
-  socket.on("disconnect",()=>{
-    console.log("a user disconnected")
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
     removeUser(socket.id);
     io.emit("getUsers", users);
-  })
-})
+  });
+});
